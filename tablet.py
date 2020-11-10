67950d4de4a7363eddd8c6462d01b412c073a1d1
susun9771 <60018589+susun9771@users.noreply.github.com>
2020-11-10 13:47:07 -0500

    Add files via upload

---
+++ tablet.py
@@ -0,0 +1,82 @@
+import tkinter as tk
+from PIL import ImageTk, Image
+import anki_vector
+
+index = 0
+img_name = ["you", "go", "stop", "want", "I", "it", "more", "not", "like"]
+
+
+def next(panel):
+    # path = "go.png"
+    global index
+    if index < len(img_name) - 1:
+        index += 1
+    path = "images/"+str(img_name[index])+".png"
+    img = ImageTk.PhotoImage(Image.open(path))
+    panel.configure(image=img)
+    panel.image = img  # keep a reference!
+
+
+def prev(panel):
+    global index
+    if index > 0:
+        index -= 1
+    path = "images/"+str(img_name[index])+".png"
+    img = ImageTk.PhotoImage(Image.open(path))
+    panel.configure(image=img)
+    panel.image = img
+
+
+def select(label):
+    stext = label.cget("text") + img_name[index] + " "
+    label.config(text=stext)
+    print(stext[17:])
+
+
+def speak(label):
+    args = anki_vector.util.parse_command_args()
+    with anki_vector.Robot(args.serial) as robot:
+        stext = label.cget("text")
+        robot.behavior.say_text(stext[17:])
+
+
+def app():
+    # Create main window
+    window = tk.Tk()
+    # divide window into two sections.
+    top = tk.Frame(window)
+    top.pack(side="top")
+    bottom = tk.Frame(window)
+    bottom.pack(side="bottom")
+
+    # place string
+    slabel = tk.Label(window, text="Vector will say: ",
+                      fg="black", relief=tk.FLAT)
+    slabel.pack()
+
+    # place image
+    #path = ["you", "go", "stop", "want", "I", "it", "more", "not", "like"]
+    img = ImageTk.PhotoImage(Image.open("images/"+str(img_name[0])+".png"))
+    panel = tk.Label(window, image=img)
+    panel.image = img
+
+    # place buttons
+    prev_button = tk.Button(window, text="Previous", width=8,
+                            height=2, command=lambda: prev(panel))
+    prev_button.pack(in_=top, side="left")
+    panel.pack(in_=top, fill="both", expand="yes", side="left")
+    next_button = tk.Button(window, text="Next", width=8,
+                            height=2, command=lambda: next(panel))
+    next_button.pack(in_=top, side="left")
+    sele_button = tk.Button(window, text="Select", width=10,
+                            height=2, command=lambda: select(slabel))
+    sele_button.pack(in_=bottom, side="left")
+    speak_button = tk.Button(window, text="Speak", width=10,
+                             height=2, command=lambda: speak(slabel))
+    speak_button.pack(in_=bottom, side="right")
+    # Start the GUI
+    window.mainloop()
+
+
+if __name__ == '__main__':
+    app()
